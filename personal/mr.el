e13a0351b97d6405b3fbafd828a52215b0868426
Moritz Roth <moritz@gmx.li>
2016-09-04 17:54:09 +0200

    Properly set up whitespace mode.
    Cleanup empty lines and trailing whitespace using the builtin (instead
    of a hook), explicitly display whitespace characters in emacs in a
    non-intrusive way.
    
    Also add some comments and restructure the code a bit.

--- personal/mr.el
+++ personal/mr.el
@@ -1,3 +1,20 @@
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-(setq prelude-whitespace 1)
-(setq prelude-clean-whitespace-on-save 1)
+;; Configure Whitespace mode
+;; Enable mode, and whitespace cleanup on save.
+(setq prelude-whitespace t)
+(setq prelude-clean-whitespace-on-save t)
+
+;; Mark lines exceeding 100 columns.
+(setq whitespace-line-column 100)
+;; Set whitespace style: cleanup empty lines / trailing whitespace, show whitespace characters.
+(setq whitespace-style '(empty trailing face lines-tail indentation::space tabs newline space-mark tab-mark newline-mark))
+;; Use spaces instead of tabs by default.
+(setq-default indent-tabs-mode nil)
+
+;; Disable guru mode
+(setq prelude-guru nil)
+
+;; Python
+;; Set tab with to 4.
+(add-hook 'python-mode-hook
+          (lambda ()
+            (setq-default tab-width 4)))
