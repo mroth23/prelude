f0e9f2153430cef3b18642d3a6fb8c24c023284e
Roth <roth@lunzer-partner.local>
2020-05-19 17:00:42 +0200

    Tweak performance settings

--- init.el
+++ init.el
@@ -97,7 +97,7 @@ This is DEPRECATED, use %s instead." prelude-modules-file))
 
 ;; reduce the frequency of garbage collection by making it happen on
 ;; each 50MB of allocated data (the default is on every 0.76MB)
-(setq gc-cons-threshold 50000000)
+(setq gc-cons-threshold most-positive-fixnum)
 
 ;; warn when opening files bigger than 100MB
 (setq large-file-warning-threshold 100000000)
